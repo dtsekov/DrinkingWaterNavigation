34eaac13f092bd12bbad9600a36ff6a2cdf2eb43
dimitardanielovtsekov <d.tsekov@alumnos.upm.es>
2025-05-25 21:19:45 +0200

    First commit

---
+++ android/app/src/main/kotlin/com/example/drinking_water_navigation/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.drinking_water_navigation
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ android/app/src/main/kotlin/es/upm/mad/btb/drinking_water_navigation/MainActivity.kt
@@ -0,0 +1,5 @@
+package es.upm.mad.btb.drinking_water_navigation
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
