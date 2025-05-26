bb4f41814c0a49a0105d184d63acd43457f26877
dimitardanielovtsekov <d.tsekov@alumnos.upm.es>
2025-05-26 10:58:31 +0200

    - Added shared preferences in splash screen

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,30 +1,95 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:logger/logger.dart';
+import 'settings_screen.dart';
 
 
 
-class SplashScreen extends StatelessWidget {
-
-  SplashScreen() {
-    print('SplashScreen Constructor');
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+class _SplashScreenState extends State<SplashScreen> {
+  final logger = Logger();
+  final _uidController = TextEditingController();
+  final _tokenController = TextEditingController();
+  @override
+  void initState() {
+    super.initState();
+    _loadPrefs();
   }
-
+  Future<void> _loadPrefs() async {
+    final prefs = await SharedPreferences.getInstance();
+    String? uid = prefs.getString('uid');
+    String? token = prefs.getString('token');
+    if (uid == null || token == null) {
+      _showInputDialog();
+    } else {
+      logger.d("UID: $uid, Token: $token");
+    }
+  }
+  Future<void> _showInputDialog() async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Enter UID and Token'),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                TextField(
+                  controller: _uidController,
+                  decoration: InputDecoration(hintText: "UID"),
+                ),
+                TextField(
+                  controller: _tokenController,
+                  decoration: InputDecoration(hintText: "Token"),
+                ),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: Text('Save'),
+              onPressed: () async {
+                final prefs = await SharedPreferences.getInstance();
+                await prefs.setString('uid', _uidController.text);
+                await prefs.setString('token', _tokenController.text);
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
   @override
   Widget build(BuildContext context) {
-
-    var logger = Logger();
-    logger.d("Debug message");
-    logger.w("Warning message!");
-    logger.e("Error message!!");
-
-
     return Scaffold(
       appBar: AppBar(
-        title: Text('Home'),
+        title: Text('HelloWorldFt for MAD'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.settings),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => SettingsScreen()),
+              );
+            },
+          ),
+        ],
       ),
       body: Center(
         child: Text('Welcome to the Home Screen!'),
       ),
     );
+  }
+  @override
+  void dispose() {
+    _uidController.dispose();
+    _tokenController.dispose();
+    super.dispose();
   }
 }
